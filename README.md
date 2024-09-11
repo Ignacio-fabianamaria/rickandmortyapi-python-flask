421b052629bad8507fa9fbd6f1c87665738ffbcc
Ignacio-fabianamaria <faby.tbe@gmail.com>
2024-09-11 10:16:43 -0300

    feat:first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# rickandmortyapi-python-flask
+<p align="center">
+  <img src="https://1000logos.net/wp-content/uploads/2022/03/Rick-and-Morty.png" alt="Rick and Morty" width="500" height="200"/>
+</p>
+
+## 📝 Descrição
+
+Este projeto foi desenvolvido como parte do módulo **Flask API do Bootcamp de Python & Django oferecido pela WoMakersCode**, em parceria com o iFood, Potência Tech e Microsoft. O objetivo do projeto é consumir a Rick and Morty API utilizando Flask e Python, implementando rotas para acessar informações sobre personagens, episódios e localizações da série animada.
---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+import urllib.request, json
+
+app = Flask(__name__)
+
+# flask --app app run
+
+
+@app.route("/")
+def get_list_characters_page():
+    url = "https://rickandmortyapi.com/api/character/"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+    return render_template("characters.html", characters=dict["results"])
+
+
+@app.route("/profile/<id>")
+def get_profile(id):
+    url = "https://rickandmortyapi.com/api/character/" + id
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+    return render_template("profile.html", profile=dict)
+
+
+@app.route("/lista")
+def get_list_elements():
+    url = "https://rickandmortyapi.com/api/character/"
+    response = urllib.request.urlopen(url)
+    characters = response.read()
+    data = json.loads(characters)
+
+    characters = []
+
+    for character in data["results"]:
+        character = {"name": character["name"], "status": character["status"]}
+        characters.append(character)
+    return {"characters": characters}
+
